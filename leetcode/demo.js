b17815752fb5a2bf98377674c688e1f9ff872f3a
luobozz <437337961@qq.com>
2021-06-09 16:15:06 +0800

    1

---
+++ leetcode/demo.js
@@ -0,0 +1,15 @@
+/**
+ * @author chenlingyu
+ */
+const ex = () => {
+    const args=0
+    const must = [];
+    return {
+        lc: () => {
+            let answer = args
+            return answer
+        }
+    }
+}
+
+console.log(ex().lc())
